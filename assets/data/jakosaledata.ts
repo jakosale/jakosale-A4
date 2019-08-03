dcdba93fcade59f23bfc6fb0b901400fe9befc8f
jakosale <52512105+jakosale@users.noreply.github.com>
2019-08-03 01:01:26 -0400

    Add files via upload

---
+++ assets/data/jakosaledata.ts
@@ -0,0 +1,8 @@
+import {Jakosale} from '../../app/jakosale';
+
+export const MYINFORMATION: Jakosale = {
+   studNo:'991500811',
+   studName:'Jeannelou Turbanos',
+   logName:'jakosale',
+   campus:'Davis',
+   assignTitle:'Assignment 4'};
